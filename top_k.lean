bf8df68161eef162c20e343e80f64bb361a9b50b
Sererim <danielleymanw@gmail.com>
2024-11-26 23:45:25 +0300

    Solved ! :)

--- top_k.lean
+++ top_k.lean
@@ -10,11 +10,19 @@ def first_out  : List Nat := [3, 2]
 def second_case : List Nat := [1, 1, 2, 2, 3, 3, 3, 3, 7, 7, 7, 7, 7, 10]
 def second_k    : Nat := 3
 
+open Ordering
+
+-- Cutom ordering
+def compare (a b : Nat × Nat) : Ordering :=
+  if a.snd < b.snd then lt
+  else if a.snd > b.snd then gt
+  else eq
+
 open Std
 
 def top_k_freq (numbers : List Nat) (k : Nat) : List (Nat × Nat) :=
-  let intermidiate := (numbers.foldl (λ acc n => acc.insert n (acc.getD n 0 + 1)) ( ∅ : HashMap Nat Nat)).toList.take k
-  intermidiate
+  let intermidiate := (numbers.foldl (λ acc n => acc.insert n (acc.getD n 0 + 1)) ( ∅ : HashMap Nat Nat)).toList
+  (intermidiate.mergeSort (λ a b => compare a b == Ordering.gt)).take k
 
 #eval (top_k_freq first_case first_k)
-#eval top_k_freq second_case 3
+#eval top_k_freq second_case second_k
